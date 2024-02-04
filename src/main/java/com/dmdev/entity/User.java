7b4bf2ac90a04f5eb0fc7e658f5c7df02e3705d0
Ruslan Satybaev <medtehnik850@gmail.com>
2024-02-04 20:15:51 +0700

    Lesson 6: Session

--- src/main/java/com/dmdev/entity/User.java
+++ src/main/java/com/dmdev/entity/User.java
@@ -3,7 +3,6 @@ package com.dmdev.entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
