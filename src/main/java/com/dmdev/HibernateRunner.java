a7d90a8d7c775b9a05e25dba155f1760b5790757
Ruslan Satybaev <medtehnik850@gmail.com>
2024-02-21 18:56:52 +0700

    Lesson 17: Logging log4j.xml

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -3,10 +3,13 @@ package com.dmdev;
 import com.dmdev.converter.BirthdayConverter;
 import com.dmdev.entity.User;
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+@Slf4j
 public class HibernateRunner {
 
     public static void main(String[] args) {
@@ -16,13 +19,28 @@ public class HibernateRunner {
         configuration.registerTypeOverride(new JsonBinaryType());
         configuration.configure();
 
+        User user1 = User
+                .builder()
+                .username("ivan3@gmail.com")
+                .firstname("Ivan")
+                .lastname("Ivanov")
+                .build();
+        log.info("User entity is in transient state, object: {}", user1);
+
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
-            session.beginTransaction();
-            User user = session.get(User.class, "ivan3@gmail.com");
-            user.setLastname("Satybaeva");
-            session.flush();
+            Transaction transaction = session.beginTransaction();
+            log.trace("Transaction is created, {}", transaction);
+
+            session.saveOrUpdate(user1);
+            log.trace("User is in persistent state: {}, session: {}", user1, session);
+
             session.getTransaction().commit();
+        } catch (Exception e) {
+            log.error("Exception occurred", e);
+            throw e;
+
         }
+        log.warn("User is in detached state {}", user1);
     }
 }
