fd59c9a31c4630e054d085e1017dd5646312c41e
Ruslan Satybaev <medtehnik850@gmail.com>
2024-02-21 12:47:24 +0700

    Lesson 11: EntityPersister

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -1,15 +1,11 @@
 package com.dmdev;
 
 import com.dmdev.converter.BirthdayConverter;
-import com.dmdev.entity.Birthday;
-import com.dmdev.entity.Role;
 import com.dmdev.entity.User;
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
-import java.time.LocalDate;
 
 public class HibernateRunner {
 
@@ -23,21 +19,6 @@ public class HibernateRunner {
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
             session.beginTransaction();
-//            User user = User
-//                    .builder()
-//                    .username("ivan90@gmail.com")
-//                    .firstname("Ivan")
-//                    .lastname("Ivanov")
-//                    .info("""
-//                            {
-//                                "name": "Ivan",
-//                                "id": 25
-//                            }
-//                            """)
-//                    .birthDate(new Birthday(LocalDate.of(2000, 1, 19)))
-//                    .role(Role.ADMIN)
-//                    .build();
-//            session.delete(user);
             User user = session.get(User.class, "ivan3@gmail.com");
 
             session.getTransaction().commit();
