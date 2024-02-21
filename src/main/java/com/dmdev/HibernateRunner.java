127a50c8d05721c24683e06af4b83da5fe48f844
Ruslan Satybaev <medtehnik850@gmail.com>
2024-02-21 12:36:43 +0700

    Lesson 10: Methods update_delete_get

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -23,21 +23,23 @@ public class HibernateRunner {
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
             session.beginTransaction();
-            User user = User
-                    .builder()
-                    .username("ivan3@gmail.com")
-                    .firstname("Ivan")
-                    .lastname("Ivanov")
-                    .info("""
-                            {
-                                "name": "Ivan",
-                                "id": 25
-                            }
-                            """)
-                    .birthDate(new Birthday(LocalDate.of(2000, 1, 19)))
-                    .role(Role.ADMIN)
-                    .build();
-            session.save(user);
+//            User user = User
+//                    .builder()
+//                    .username("ivan90@gmail.com")
+//                    .firstname("Ivan")
+//                    .lastname("Ivanov")
+//                    .info("""
+//                            {
+//                                "name": "Ivan",
+//                                "id": 25
+//                            }
+//                            """)
+//                    .birthDate(new Birthday(LocalDate.of(2000, 1, 19)))
+//                    .role(Role.ADMIN)
+//                    .build();
+//            session.delete(user);
+            User user = session.get(User.class, "ivan3@gmail.com");
+
             session.getTransaction().commit();
         }
     }
--- src/main/java/com/dmdev/entity/User.java
+++ src/main/java/com/dmdev/entity/User.java
@@ -1,15 +1,14 @@
 package com.dmdev.entity;
 
-import com.dmdev.converter.BirthdayConverter;
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.Column;
-import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -22,22 +21,18 @@ import javax.persistence.Table;
 @Builder
 @Entity
 @Table(name = "users", schema = "public")
+@TypeDef(name = "dmdev", typeClass = JsonBinaryType.class)
 public class User {
+
     @Id
-    @Column(name = "username", nullable = false, length = 128)
     private String username;
-
-    @Column(name = "firstname", length = 128)
     private String firstname;
-
-    @Column(name = "lastname", length = 128)
     private String lastname;
 
     @Column(name = "birth_date")
     private Birthday birthDate;
 
-    @Type(type = "jsonb")
-    @Column(name = "info")
+    @Type(type = "dmdev")
     private String info;
 
     @Enumerated(EnumType.STRING)
