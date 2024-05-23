21280e7e26c81e6a4bc909de9c205a5dafab7538
Ruslan Satybaev <medtehnik850@gmail.com>
2024-05-23 19:57:32 +0700

    Lesson 35: ManyToMany

---
+++ src/main/java/com/dmdev/entity/Chat.java
@@ -0,0 +1,41 @@
+package com.dmdev.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "name")
+@ToString(exclude = "users")
+@Table(name = "chat", schema = "public")
+public class Chat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    @Builder.Default
+    @ManyToMany(mappedBy = "chats")
+    private Set<User> users = new HashSet<>();
+}
